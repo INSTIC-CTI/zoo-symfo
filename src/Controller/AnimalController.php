abfcf7582985e6f35440f31cebe4b43e5893b447
Rodolphe <dutchr@live.fr>
2021-11-02 16:55:45 +0100

    :art: Display animals

--- src/Controller/AnimalController.php
+++ src/Controller/AnimalController.php
@@ -2,17 +2,20 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\Animal;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class AnimalController extends AbstractController
 {
     #[Route('/', name: 'home')]
     public function index(): Response
     {
+      $repository = $this->getDoctrine()->getRepository(Animal::class);
+      $animaux = $repository->findAll();
         return $this->render('animal/index.html.twig', [
-        
+        "animaux" => $animaux,
         ]);
     }
     #[Route('/animal', name: 'animals')]
